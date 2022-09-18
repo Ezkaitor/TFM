65f6578b97e5364507799be6a0454c0c6c44f7b5
Aitor <100422687@alumnos.uc3m.es>
2022-09-18 02:09:05 +0200

    +CHG smoothen algorithm, +CHG visualization commands

--- drone-test/src/create_occupation_map.py
+++ drone-test/src/create_occupation_map.py
@@ -62,7 +62,7 @@ class ImageOccupation:
                             "Goal": False,
                             }
         self.truth_map2 = {
-                            "Processed": False,
+                            "Processed": True,
                             "Empty": False,
                             }
         
@@ -139,11 +139,11 @@ class ImageOccupation:
             #if self.last_occupation_image is not None:
             #    self.occupation_image += self.last_occupation_image
             #self.last_occupation_image = temp
-            
+            #rospy.loginfo(f"Camera updated {time.time()}")
             self.truth_map["BBoxes"] = True
     
     def send_empty(self):
-        if not self.truth_map2["Empty"] and (time.time()-self.no_detect > 1):
+        if not self.truth_map2["Empty"]: # and (time.time()-self.no_detect > 0.1):
             new_target = PoseStamped()
             new_target.header.frame_id = "none"
             self.new_target_pub.publish(new_target)
@@ -153,13 +153,24 @@ class ImageOccupation:
             self.no_detect = time.time()
 
             self.new_goal = None
-        
+    
+    def show(self):
+        if self.truth_map["BBoxes"] and VISUALIZE and all(flag for flag in self.truth_map.values()) and np.any(self.occupation_image):
+            self.occupation_image_show = cv2.circle(self.occupation_image_show, (self.cam_goal_x, self.cam_goal_y), radius=5, color=(0, 0, 255), thickness=-1)
+            self.occupation_image_show = cv2.resize(self.occupation_image_show, (400, 400))
+            cv2.imshow("Occupation", self.occupation_image_show)
+            cv2.waitKey(1)
+
     def check(self):
 
-
-        #self.goal_point = Point(10, 1, 2)
+        if self.truth_map["BBoxes"] and VISUALIZE:
+            # Showing purposes
+            self.occupation_image_show = (self.occupation_image*255).astype(np.uint8)
+            self.occupation_image_show = cv2.cvtColor(self.occupation_image_show, cv2.COLOR_GRAY2BGR)
+        #self.goal_point = Point(60, 50, 1.5)
         #self.truth_map["Goal"] = True
         #if False:
+        self.truth_map2["Processed"] = False
         if all(flag for flag in self.truth_map.values()) and np.any(self.occupation_image): # TODO: set to false when goal reached
                                                             # All cameras is not necessary, just 1
 
@@ -175,62 +186,77 @@ class ImageOccupation:
             module_pitch = math.asin(np.dot(dir_vector, xy_normal) / (math.sqrt(np.dot(dir_vector, dir_vector)) * math.sqrt(np.dot(xy_normal, xy_normal))))
             
             # Calculate goal coordinates in image
-            cam_goal_x = int((self.current_orientation[2] - module_yaw) * (180.0/math.pi)*self.pixel_per_degree + self.occupation_image.shape[1]/2)
-            cam_goal_y = int((module_pitch - self.current_orientation[1]) * (180.0/math.pi)*self.pixel_per_degree + self.occupation_image.shape[0]/2)
+            self.cam_goal_x = int((self.current_orientation[2] - module_yaw) * (180.0/math.pi)*self.pixel_per_degree + self.occupation_image.shape[1]/2)
+            self.cam_goal_y = int((self.current_orientation[1] - module_pitch) * (180.0/math.pi)*self.pixel_per_degree + self.occupation_image.shape[0]/2)
 
-            if cam_goal_x >= self.occupation_image.shape[1] or cam_goal_x < 0 or cam_goal_y >= self.occupation_image.shape[0] or cam_goal_x < 0:
+            if self.cam_goal_x >= self.occupation_image.shape[1] or self.cam_goal_x < 0 or self.cam_goal_y >= self.occupation_image.shape[0] or self.cam_goal_x < 0:
+                self.truth_map2["Processed"] = True
                 return False
             
             ## NEW ##
             # Angle between two vectors
             if self.new_goal is not None:
                 module_yaw_new = math.atan2(self.new_goal.y-self.current_position.y, self.new_goal.x-self.current_position.x) 
-                #module_pitch_new = math.atan2(self.new_goal.z-self.current_position.z, self.new_goal.x-self.current_position.x)
                 # Angle between plane xy and vector
                 dir_vector_new = to_numpy(self.new_goal) - to_numpy(self.current_position)
                 xy_normal = np.array([0,0,1])
                 module_pitch_new = math.asin(np.dot(dir_vector_new, xy_normal) / (math.sqrt(np.dot(dir_vector_new, dir_vector_new)) * math.sqrt(np.dot(xy_normal, xy_normal))))
 
                 # Calculate goal coordinates in image
-                cam_goal_x_new = int((self.current_orientation[2] - module_yaw_new) * (180.0/math.pi)*self.pixel_per_degree + self.occupation_image.shape[1]/2)
-                cam_goal_y_new = int((module_pitch_new - self.current_orientation[1]) * (180.0/math.pi)*self.pixel_per_degree + self.occupation_image.shape[0]/2)
+                self.cam_goal_x_new = int((self.current_orientation[2] - module_yaw_new) * (180.0/math.pi)*self.pixel_per_degree + self.occupation_image.shape[1]/2)
+                self.cam_goal_y_new = int((self.current_orientation[1] - module_pitch_new) * (180.0/math.pi)*self.pixel_per_degree + self.occupation_image.shape[0]/2)
 
             
-            self.truth_map2["Processed"] = False
+            
             
             self.proc_count += 1
-            print(f"Pre process {self.proc_count}: {time.time()-self.detected}")
-            self.detected = time.time()
-            full, gradient = create_safe_zone(self.occupation_image, 250)
+            #print(f"Pre process {self.proc_count}: {time.time()-self.detected}")
+            self.truth_map2["Processed"] = True
+            full, gradient = create_safe_zone(self.occupation_image, 150)
+            
 
+            #rospy.loginfo(f"Trying to {time.time()}")
 
-            if full[cam_goal_y, cam_goal_x]:
-
+            if full[self.cam_goal_y, self.cam_goal_x]:
                 if self.new_goal is not None:
-                    if cam_goal_x_new >= self.occupation_image.shape[1] or cam_goal_x_new < 0 or cam_goal_y_new >= self.occupation_image.shape[0] or cam_goal_x_new < 0:
-                        return True
-                    if not full[cam_goal_y_new, cam_goal_x_new]: return True
+                    if self.cam_goal_x_new <= self.occupation_image.shape[1] and self.cam_goal_x_new > 0 and self.cam_goal_y_new <= self.occupation_image.shape[0] and self.cam_goal_x_new > 0:
+                        if not full[self.cam_goal_y_new, self.cam_goal_x_new]:
+                            return True
                 
                 self.main_goal = self.goal_point
                 
-                if self.current_position.z < 4:
-                    gradient[int(800-(500-self.current_position.z*100)-1):, :] = False # Remove part of the floor
+                xy_module = 15#math.sqrt(np.dot(dir_vector[:2], dir_vector[:2]))
 
-                print(f"Pre calculation {self.proc_count}: {time.time()-self.detected}")
+                min_height = 0.5
+                
+                delta_pitch_rev = math.atan(xy_module/(min_height-self.current_position.z))
+                cam_cut = int(self.occupation_image.shape[0]/2 - delta_pitch_rev*self.pixel_per_degree)
+                
+                
+                gradient[cam_cut:, :] = False # Remove part of the floor
+                if not np.any(gradient):
+                    
+                    return True
+
+                #print(f"Pre calculation {self.proc_count}: {time.time()-self.detected}")
                 self.detected = time.time()
-                closest_goal, dist = closest_from_boolmap([cam_goal_y, cam_goal_x], gradient)
+                closest_goal, dist = closest_from_boolmap([self.cam_goal_y, self.cam_goal_x], gradient)
 
                 if self.new_goal is not None:
-                    _, dist_new = closest_from_boolmap([cam_goal_y_new, cam_goal_x_new], gradient)
-                    if dist_new < dist: return True
+                    _, dist_new = closest_from_boolmap([self.cam_goal_y_new, self.cam_goal_x_new], gradient)
+                    if dist_new < dist:
+                        
+                        return True
 
-                xy_module = math.sqrt(np.dot(dir_vector[:2], dir_vector[:2]))
+                
 
                 delta_yaw = (closest_goal[1] - self.occupation_image.shape[1]/2)/self.pixel_per_degree
                 delta_pitch = (self.occupation_image.shape[0]/2 - closest_goal[0])/self.pixel_per_degree
                 
                 new_goal_x = (xy_module/math.sin(math.radians(180-90-abs(delta_yaw))))*math.sin(math.radians(delta_yaw))
-                new_goal_y = (xy_module/math.sin(math.radians(180-90-abs(delta_pitch))))*math.sin(math.radians(delta_pitch)) + self.current_position.z
+                if (delta_pitch < 10 and delta_pitch > 0) or (delta_pitch > -10 and delta_pitch < 0): new_goal_y = 0
+                else:
+                    new_goal_y = (xy_module/math.sin(math.radians(180-90-abs(delta_pitch))))*math.sin(math.radians(delta_pitch))
                 
                 #self.goal_point[0] = self.current_position[0] - 4
                 #new_goal_x = (closest_goal[1] - self.image_size[1]/2) * (self.goal_point.x*math.sin(math.radians(self.FOVx/2))/math.sin(math.radians(270))) / (self.image_size[0]/2) + self.current_position.x
@@ -240,9 +266,13 @@ class ImageOccupation:
                 new_target.header.frame_id = "temp_target"
                 new_target.header.stamp = rospy.Time.now()
                 
-                new_target.pose.position.x = self.goal_point.x + new_goal_x*math.sin(module_yaw)
-                new_target.pose.position.y = self.goal_point.y - new_goal_x*math.cos(module_yaw)
-                new_target.pose.position.z = new_goal_y
+                if self.current_position.x - self.goal_point.x > 0: x_module = 5
+                else: x_module = -5
+                if self.current_position.y - self.goal_point.y > 0: x_module = 5
+                else: y_module = -5
+                new_target.pose.position.x = self.goal_point.x + new_goal_x*math.sin(module_yaw) #self.goal_point.x + new_goal_x*math.sin(module_yaw)
+                new_target.pose.position.y = self.goal_point.y - new_goal_x*math.cos(module_yaw)#self.goal_point.y - new_goal_x*math.cos(module_yaw)
+                new_target.pose.position.z = self.current_position.z + new_goal_y
 
                 self.new_goal = new_target.pose.position
 
@@ -255,32 +285,31 @@ class ImageOccupation:
             
                 
                 if VISUALIZE:
-                    # Showing purposes
-                    self.occupation_image_show = (self.occupation_image*255).astype(np.uint8)
-                    self.occupation_image_show = cv2.cvtColor(self.occupation_image_show, cv2.COLOR_GRAY2BGR)
-                    self.occupation_image_show = cv2.circle(self.occupation_image_show, (cam_goal_x, cam_goal_y), radius=5, color=(0, 0, 255), thickness=-1)
                     self.occupation_image_show = cv2.circle(self.occupation_image_show, (closest_goal[1], closest_goal[0]), radius=5, color=(255, 0, 0), thickness=-1)
                     self.occupation_image_show[gradient] = (0,255,0)
-
-                    #cv2.imshow("Gradient", gradient)
-                    self.occupation_image_show = cv2.resize(self.occupation_image_show, (700, 400))
+                    self.occupation_image_show = cv2.circle(self.occupation_image_show, (self.cam_goal_x, self.cam_goal_y), radius=5, color=(0, 0, 255), thickness=-1)
+                    self.occupation_image_show = cv2.resize(self.occupation_image_show, (400, 400))
                     cv2.imshow("Occupation", self.occupation_image_show)
                     cv2.waitKey(1)
+
+                    #cv2.imshow("Gradient", gradient)
+                    
                 
-                self.truth_map2["Processed"] = True
+                
                 self.no_detect = time.time()
-                self.occupation_image = np.zeros((self.occupation_image.shape), dtype=bool)
+                #self.occupation_image = np.zeros((self.occupation_image.shape), dtype=bool)
                 #self.truth_map["AllCameras"] = False
                 
                 return True
         
         if self.truth_map["BBoxes"] and VISUALIZE: 
             self.occupation_image_show = (self.occupation_image*255).astype(np.uint8)
-            self.occupation_image_show = cv2.resize(self.occupation_image_show, (700, 400))
+            self.occupation_image_show = cv2.resize(self.occupation_image_show, (400, 400))
             cv2.imshow("Occupation", self.occupation_image_show)
             cv2.waitKey(1)
-            self.occupation_image = np.zeros((self.occupation_image.shape), dtype=bool)
-        self.truth_map2["Processed"] = True
+            #self.occupation_image = np.zeros((self.occupation_image.shape), dtype=bool)
+            self.truth_map2["Processed"] = True
+        
         
         return False
 
@@ -293,6 +322,8 @@ def main():
     img_occ = ImageOccupation()
     while not rospy.is_shutdown():
         success = img_occ.check()
+        img_occ.truth_map2["Processed"] = True
+        img_occ.show()
         if not success: img_occ.send_empty()
         rate.sleep()
 
